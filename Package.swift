5b7dc6806bf7b222dfdae79408979d0249daf79f
Pasi Salenius <pasi@freshbits.fi>
2019-09-27 10:27:19 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["ZippyJSON"]),
     ],
     dependencies: [
-        .package(path: "../ZippyJSONCFamily"),
-        .package(path: "../JJLISO8601DateFormatter")
+        .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.2")),
+        .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
