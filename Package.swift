f986b3348e2fa8a03c30c9499c74365dfee3160b
Pasi Salenius <pasi@freshbits.fi>
2019-09-27 10:29:19 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ZippyJSON",
-            dependencies: ["ZippyJSONCFamily", "JJLISO8601DateFormatter"]),
-        .testTarget(
-            name: "ZippyJSONTests",
-            dependencies: ["ZippyJSON"]),
+            dependencies: ["ZippyJSONCFamily", "JJLISO8601DateFormatter"])
     ]
 )
